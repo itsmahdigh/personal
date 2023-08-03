0f27a75e6a053bf05dcb7c6290c00ace3d497ec6
mahdigh621 <mahdigh621@gmail.com>
2023-08-03 20:12:59 +0330

    1402-05-12/4

--- src/Pages/landingPage.jsx
+++ src/Pages/landingPage.jsx
@@ -2,12 +2,14 @@ import Navbar from "../navBar/Navbar";
 import SectionOne from "../firstPageComponent/sectionOne";
 import SocialMedia from "../firstPageComponent/socialMedia";
 import AboutMe from "../firstPageComponent/aboutMe";
+import Footer from "../Footer/Footer";
 function landingPage(){
     return<>
    <Navbar />
    <SectionOne /> 
    <SocialMedia />
    <AboutMe />
+   <Footer/>
     </>
 }
 export default landingPage;
--- src/firstPageComponent/aboutMe.jsx
+++ src/firstPageComponent/aboutMe.jsx
@@ -1,24 +1,57 @@
 import Pic from '../photo_2023-08-03_11-22-36.jpg'
+import Img from '../photo_2023-08-03_11-22-44.jpg'
+import Picture from '../photo_2023-08-03_11-22-50.jpg'
 function aboutMe() {
 
     return <>
-        <div className="container mx-auto rounded-lg lg:h-[500px] md:h-[400px] sm:h-[300px]  mt-8 grid grid-cols-2">
-            <div className="title ">
-                <img src={Pic} alt="" className='lg:h-[500px] md:h-[400px] sm:h-[300px] rounded-lg' />
+        <div className="container mx-auto    mt-8 grid grid-cols-2">
+            <div className="title flex justify-center">
+                <img src={Pic} alt="" className='lg:h-[400px] md:h-[300px] sm:h-[200px] rounded-2xl ' />
             </div>
             <div>
-            <div className='flex  justify-between  mt-4 mr-4' dir='rtl'>
-                <span class="flex w-5 h-3 hover:h-5 animation-bounce bg-gray-500 rounded-full"></span>
-                <span class="flex w-3 h-5 hover:w-5 bg-blue-600 rounded-full"></span>
-                <span class="flex w-5 h-3 hover:h-5 bg-green-500 rounded-full"></span>
-                <span class="flex w-3 h-5 hover:w-5 bg-purple-500 rounded-full"></span>
-                <span class="flex w-5 h-3 hover:h-5 bg-indigo-500 rounded-full"></span>
-                <span class="flex w-3 h-5 hover:w-5 bg-teal-500 rounded-full"></span>
+                <div className='flex  justify-between  mt-4 mr-4' dir='rtl'>
+                    <span class="flex w-5 h-3 hover:h-5 animation-bounce bg-gray-500 rounded-full"></span>
+                    <span class="flex w-3 h-5 hover:w-5 bg-blue-600 rounded-full"></span>
+                    <span class="flex w-5 h-3 hover:h-5 bg-green-500 rounded-full"></span>
+                    <span class="flex w-3 h-5 hover:w-5 bg-purple-500 rounded-full"></span>
+                    <span class="flex w-5 h-3 hover:h-5 bg-indigo-500 rounded-full"></span>
+                    <span class="flex w-3 h-5 hover:w-5 bg-teal-500 rounded-full"></span>
+                </div >
+                <div className='flex justify-center text-center text-2xl items-center'>
+                    <span className=''>   متولد 11مردادماه 1380 شهرستان میانه اذربایجان شرقی <br />
+                        دانشجوی کارشناسی مهندسی کامپیوتر دانشگاه سراسری زنجان </span>
                 </div>
-                <span className='flex justify-center text-center text-2xl items-center'>   متولد 11مردادماه 1380 شهرستان میانه اذربایجان شرقی <br />دانشجوی کارشناسی مهندسی کامپیوتر دانشگاه سراسری زنجان </span>
+            </div>
+        </div>
+        {/* <div className='container mx-auto flex  justify-between  mt-4 mr-4' dir='rtl'>
+            <span class="flex w-5 h-3 hover:h-5 animation-bounce bg-gray-500 rounded-full"></span>
+            <span class="flex w-3 h-5 hover:w-5 bg-blue-600 rounded-full"></span>
+            <span class="flex w-5 h-3 hover:h-5 bg-green-500 rounded-full"></span>
+            <span class="flex w-3 h-5 hover:w-5 bg-purple-500 rounded-full"></span>
+            <span class="flex w-5 h-3 hover:h-5 bg-indigo-500 rounded-full"></span>
+            <span class="flex w-3 h-5 hover:w-5 bg-teal-500 rounded-full"></span>
+        </div > */}
+        <div className='grid grid-cols-2 container mx-auto'>
+
+            <div>
+
+            </div>
+            <div className='flex justify-center'>
+                <img src={Img} alt="" className='lg:h-[400px] md:h-[300px] sm:h-[200px] rounded-2xl ' />
+
             </div>
         </div>
 
+        <div className='grid grid-cols-2 container mx-auto mb-4'>
+
+            <div className='flex justify-center'>
+                <img src={Picture} alt="" className='lg:h-[400px] md:h-[300px] sm:h-[200px] rounded-2xl ' />
+            </div>
+            <div >
+
+
+            </div>
+        </div>
     </>
 }
 export default aboutMe;
